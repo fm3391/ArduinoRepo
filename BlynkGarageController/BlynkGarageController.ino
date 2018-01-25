5231f4008c33943320ae9c40cae4df5e9c865742
Aaron <fm3391@gmail.com>
2018-01-24 19:50:56 -0600

    Simple Implementation

--- BlynkGarageController/BlynkGarageController.ino
+++ BlynkGarageController/BlynkGarageController.ino
@@ -1,5 +1,3 @@
-#define BLYNK_PRINT Serial
-
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
@@ -8,36 +6,52 @@ char auth[] = "34d1777241174d91b97afb9a77dd55fa";
 char ssid[] = "NETGEAR36_ATT";
 char pass[] = "asdf1234ASDF!@#$";
 
-int relayPin = 16;
-int sensorOpenPin = 5;
-int sensorClosedPin = 4;
+const int relayPin = 16;
+const int sensorOpenPin = 5;
+const int sensorClosedPin = 4;
+
+enum class Color {RED, GREEN, BLUE};
+
+Color color;
 
 BLYNK_WRITE(V1)
 {
   int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
-  if(pinValue == 1){
+  if (pinValue == 1) {
     digitalWrite(relayPin, LOW);
-  }else{
+  } else {
     digitalWrite(relayPin, HIGH);
   }
 }
 
+BLYNK_READ(V0)
+{
+  int sensorOpenVal = digitalRead(sensorOpenPin);
+  int sensorClosedVal = digitalRead(sensorClosedPin);
+  String state = "";
+
+  if (sensorOpenVal == 1) {
+    state = "OPEN";
+  } else if (sensorClosedVal == 1) {
+    state = "CLOSED";
+  } else {
+    state = "TRANSISTIONING";
+  }
+  Serial.println("State: " + state);
+  Blynk.virtualWrite(V0, state);
+}
+
 
 void setup() {
-  Serial.begin(9600);
   Blynk.begin(auth, ssid, pass);
 
-  pinMode(relayPin,OUTPUT);
-  pinMode(sensorOpenPin,INPUT);
-  pinMode(sensorClosedPin,INPUT);
+  pinMode(relayPin, OUTPUT);
+  pinMode(sensorOpenPin, INPUT);
+  pinMode(sensorClosedPin, INPUT);
 
   digitalWrite(relayPin, HIGH);
 }
 
 void loop() {
   Blynk.run();
-  Serial.print("Sensor 1: "+ String(digitalRead(sensorOpenPin)));
-  Serial.print(" / ");
-  Serial.println("Sensor 2: "+ String(digitalRead(sensorClosedPin)));
-  delay(1000);
 }
